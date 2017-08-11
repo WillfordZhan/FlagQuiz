15434eebe8cf25e26766603d59a885d7d493993f
tinyWillford <willfordzhan@gmail.com>
2017-08-11 10:53:06 +0800

    Import packages in MainActivity.java Now sava and go back to find the R problem

--- app/src/main/java/com/github/willfordzhan/flagquiz/MainActivity.java
+++ app/src/main/java/com/github/willfordzhan/flagquiz/MainActivity.java
@@ -1,5 +1,10 @@
 package com.github.willfordzhan.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
 
